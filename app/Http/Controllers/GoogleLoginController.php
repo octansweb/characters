2e2f3a4a09bbe3cb5951c2e50f425192e57aa106
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-11-07 04:01:18 +0000

    Update

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -19,17 +19,16 @@ class GoogleLoginController extends Controller
 
     public function handleGoogleCallback(Request $request)
     {
-        dd($request->all());
-
         $googleUser = Socialite::driver('google')->stateless()->user();
 
-        $user = User::where('email', $googleUser->email)->first();
-        if (!$user) {
-            $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => Hash::make(rand(100000, 999999))]);
-        }
+        dd($googleUser);
+        // $user = User::where('email', $googleUser->email)->first();
+        // if (!$user) {
+        //     $user = User::create(['name' => $googleUser->name, 'email' => $googleUser->email, 'password' => Hash::make(rand(100000, 999999))]);
+        // }
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect('/dashboard');
+        // return redirect('/dashboard');
     }
 }
