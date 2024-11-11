d769809717d9075aa0076b4dff0fded8eb85ab10
Rana Faiz Ahmad <ranafaizahmad1@gmail.com>
2024-11-11 01:17:26 +0000

    Update

--- app/Http/Controllers/GoogleLoginController.php
+++ app/Http/Controllers/GoogleLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Jenssegers\Agent\Agent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -20,19 +21,31 @@ class GoogleLoginController extends Controller
     public function handleGoogleCallback(Request $request)
     {
         $user = Socialite::driver('google')->user();
+        $agent = new Agent();
 
         // Check if the user already exists by email:
         $existingUser = User::whereEmail($user->email)->first();
         if ($existingUser) {
-            // If we already have the user, simply log them in and redirect.
+            // If we already have the user, log them in and redirect.
             Auth::login($existingUser);
-            return redirect(route('dashboard'));
+        } else {
+            // Register a new user with random password
+            $newUser = User::create([
+                'name' => $user->name,
+                'email' => $user->email,
+                'password' => Hash::make(rand(100000, 999999)),
+            ]);
+            $newUser->markEmailAsVerified();
+            Auth::login($newUser);
         }
 
-        // Register a new user with random password
-        $newUser = User::create(['name' => $user->name, 'email' => $user->email, 'password' => Hash::make(rand(100000, 999999))]);
-        Auth::login($newUser);
+        // Check if the user is on a mobile device
+        if ($agent->isMobile()) {
+            // Redirect to the mobile URL from the environment file
+            return redirect()->away(env('GOOGLE_MOBILE_CALLBACK'));
+        }
 
+        // Otherwise, redirect to the dashboard
         return redirect(route('dashboard'));
     }
 }
